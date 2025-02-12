f08ecaeb6fb78f8ee8d69c7698a94d22350bbf45
eatsosoo <eatsoosoo@gmail.com>
2025-02-12 11:13:05 +0700

    feat(homework): add EditorHomeworkModal component and integrate with class table

---
+++ src/views/exercise/EditorHomeworkModal.vue
@@ -0,0 +1,63 @@
+<template>
+  <BasicModal
+    v-bind="$attrs"
+    :title="props.titleEditor"
+    default-fullscreen
+    :can-fullscreen="false"
+    :loading="loading"
+    @ok="handleOk"
+  >
+    <Reading ref="readingRef" :value="detail?.reading" />
+  </BasicModal>
+</template>
+<script lang="ts" setup>
+  import { BasicModal } from '@/components/Modal';
+  import Reading from './reading.vue';
+  import { ref, watch } from 'vue';
+  import { examDetailApi } from '@/api/exam/exam';
+  import { useMessage } from '@/hooks/web/useMessage';
+  import { useI18n } from '@/hooks/web/useI18n';
+  import { ExamDetailItem } from '@/api/exam/examModel';
+
+  const props = defineProps({
+    classId: {
+      type: Number,
+      default: null,
+    },
+    titleEditor: {
+      type: String,
+      default: '',
+    },
+  });
+  const readingRef = ref<InstanceType<typeof Reading> | null>(null);
+  const detail = ref<ExamDetailItem | null>(null);
+  const loading = ref(false);
+
+  const { t } = useI18n();
+  const { createMessage } = useMessage();
+
+  function handleOk() {
+    // if (readingRef.value && props.examId) {
+    //   readingRef.value.submitAll(props.examId);
+    // }
+  }
+
+  async function getExamDetail(examId: number) {
+    try {
+      loading.value = true;
+      const result = await examDetailApi(examId);
+      detail.value = result;
+    } catch (error) {
+      createMessage.error(t('sys.app.dataNotFound'));
+      console.log(error);
+    } finally {
+      loading.value = false;
+    }
+  }
+  watch(
+    () => props.classId,
+    (value) => {
+      getExamDetail(value);
+    },
+  );
+</script>
---
+++ src/views/exercise/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <BasicTable @register="registerTable">
+      <template #form-custom> custom-slot </template>
+      <template #toolbar>
+        <a-button type="primary" @click="getFormValues">{{ t('table.enterData') }}</a-button>
+      </template>
+      <template #bodyCell="{ column, record }">
+        <template v-if="column.key === 'start_date'">
+          {{ record.start_date.split(' ')[0] }}
+        </template>
+        <template v-if="column.key === 'end_date'">
+          {{ record.end_date.split(' ')[0] }}
+        </template>
+        <template v-if="column.key === 'action'">
+          <a-button
+            size="small"
+            preIcon="ant-design:user-add-outlined"
+            @click="openHomeworkModal"
+          />
+        </template>
+      </template>
+    </BasicTable>
+
+    <EditorHomeworkModal
+      :title="titleModal"
+      :class-id="targetClass?.id"
+      @register="registerHomeworkModal"
+    />
+  </div>
+</template>
+<script lang="ts" setup>
+  import { BasicTable, useTable } from '@/components/Table';
+  import { getClassColumns, getFormConfig } from '@/views/classroom/tableData';
+  import { useI18n } from '@/hooks/web/useI18n';
+  import { useModal } from '@/components/Modal';
+  import { computed, ref } from 'vue';
+  import { classListApi } from '@/api/class/class';
+  import { ClassListItem } from '@/api/class/classModel';
+  import EditorHomeworkModal from './EditorHomeworkModal.vue';
+
+  const { t } = useI18n();
+  const [registerHomeworkModal, { openModal: openHomeworkModal }] = useModal();
+  const [registerTable, { getForm }] = useTable({
+    title: t('routes.page.classroomList'),
+    api: classListApi(),
+    columns: getClassColumns(),
+    useSearchForm: true,
+    formConfig: getFormConfig(),
+    showTableSetting: true,
+    tableSetting: { fullScreen: true },
+    showIndexColumn: false,
+    rowKey: 'id',
+    showSelectionBar: false,
+    actionColumn: {
+      title: t('table.action'),
+      dataIndex: 'action',
+    },
+  });
+
+  const targetClass = ref<ClassListItem | null>(null);
+  const titleModal = computed(() => {
+    return targetClass.value
+      ? `Class ${targetClass.value.title} - ${t('routes.page.assignHomework')}`
+      : '';
+  });
+
+  function getFormValues() {
+    console.log(getForm().getFieldsValue());
+  }
+</script>
