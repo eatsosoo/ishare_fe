1bfafba3c544895c6e8febdd3ff3005fc384d1cb
eatsosoo <eatsoosoo@gmail.com>
2025-02-26 16:19:19 +0700

    export excel

--- src/views/student/outcomes.vue
+++ src/views/student/outcomes.vue
@@ -15,34 +15,11 @@
   </div>
 </template>
 <script lang="ts" setup>
-  import BasicTable from '@/components/Table/src/BasicTable.vue';
-  import { FormSchema, useTable } from '@/components/Table';
-  import { useI18n } from '@/hooks/web/useI18n';
-  import { getExamColumns, getSearchExamOfStudentConfig } from '@/views/classroom/tableData';
-  import { Tag, Card } from 'ant-design-vue';
-  import { getExamCapacityApi } from '@/api/student/student';
+  import { FormSchema } from '@/components/Table';
+  import { Card } from 'ant-design-vue';
   import { useForm, BasicForm } from '@/components/Form';
   import { getToken } from '@/utils/auth';
-  import { getUserInfo } from '@/api/sys/user';
   import { useUserStore } from '@/store/modules/user';
-
-  const { t } = useI18n();
-
-  const [registerTable] = useTable({
-    api: getExamCapacityApi(),
-    columns: getExamColumns(),
-    useSearchForm: true,
-    formConfig: getSearchExamOfStudentConfig(),
-    showTableSetting: false,
-    tableSetting: { fullScreen: true },
-    showIndexColumn: false,
-    rowKey: 'id',
-    showSelectionBar: false,
-    actionColumn: {
-      title: t('table.action'),
-      dataIndex: 'action',
-    },
-  });
 
   const resutlStudy: FormSchema[] = [
     {
